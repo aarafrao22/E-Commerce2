e285dcb18b016da3ecb8843fc58fccf42ec972e6
Rao Aaraf Younus <aarafrao22@gmail.com>
2022-12-22 20:12:19 +0500

    MainLayoutSet

---
+++ app/src/main/java/com/aarafrao/ecommerceapp/MainActivity3.java
@@ -0,0 +1,51 @@
+package com.aarafrao.ecommerceapp;
+
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.aarafrao.ecommerceapp.databinding.ActivityMain3Binding;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    private ActivityMain3Binding binding;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMain3Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main3);
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+
+}
---
+++ app/src/main/java/com/aarafrao/ecommerceapp/ui/dashboard/DashboardFragment.java
@@ -0,0 +1,94 @@
+package com.aarafrao.ecommerceapp.ui.dashboard;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.aarafrao.ecommerceapp.R;
+import com.aarafrao.ecommerceapp.databinding.FragmentDashboardBinding;
+import com.aarafrao.ecommerceapp.myadaptercard;
+import com.aarafrao.ecommerceapp.ProductModel;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class DashboardFragment extends Fragment {
+
+    private FragmentDashboardBinding binding;
+    RecyclerView recyclerView;
+    myadaptercard myadpater;
+    ArrayList<ProductModel> list;
+    SharedPreferences sharedPreferences;
+    String MyPREFERENCES = "MyPrefs";
+    TextView totalamount;
+    TextView countitem;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+
+        binding = FragmentDashboardBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        recyclerView = root.findViewById(R.id.productlistcard);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        totalamount = root.findViewById(R.id.totalamount);
+        countitem = root.findViewById(R.id.itemtext);
+        list = new ArrayList<>();
+        myadpater = new myadaptercard(getContext(), list);
+        recyclerView.setAdapter(myadpater);
+
+        sharedPreferences = getContext().getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+
+        Map<String, ?> keys = sharedPreferences.getAll();
+        int total = 0;
+        int count = 0;
+        for (Map.Entry<String, ?> entry : keys.entrySet()) {
+            Log.d("map values", entry.getKey() + ": " +
+                    entry.getValue().toString());
+
+            Type type = new TypeToken<List<String>>() {
+            }.getType();
+            Gson gson = new Gson();
+            List<String> arrPackageData = gson.fromJson(entry.getValue().toString(), type);
+            ProductModel pd = new ProductModel();
+            pd.setPrice(Integer.parseInt(arrPackageData.get(0)));
+            pd.setProduct_desc(arrPackageData.get(1));
+            pd.setProduct_name(arrPackageData.get(3));
+            pd.setUrl(arrPackageData.get(2));
+            pd.setQuantity(Integer.parseInt(arrPackageData.get(4)));
+            list.add(pd);
+            total += Integer.parseInt(arrPackageData.get(0)) * Integer.parseInt(arrPackageData.get(4));
+            count += 1;
+        }
+
+        myadpater.notifyDataSetChanged();
+        total += 3.95;
+        totalamount.setText(String.valueOf(total));
+        countitem.setText(String.valueOf(count) + " item");
+
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
